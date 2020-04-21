710480f42dc72e4555b5e91cc32da806a9ea10dd
Ilya Kaznacheev <ilya@kaznacheev.me>
2020-04-21 12:45:57 +0300

    Fix env-default overwrite issue Closes #40
    Signed-off-by: Ilya Kaznacheev <ilya@kaznacheev.me>

--- cleanenv.go
+++ cleanenv.go
@@ -19,6 +19,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -174,6 +175,11 @@ type structMeta struct {
 	separator   string
 	description string
 	updatable   bool
+}
+
+// isFieldValueZero determines if fieldValue empty or not
+func (sm *structMeta) isFieldValueZero() bool {
+	return isZero(sm.fieldValue)
 }
 
 // readStructMetadata reads structure metadata (types, tags, etc.)
@@ -517,5 +523,44 @@ func FUsage(w io.Writer, cfg interface{}, headerText *string, usageFuncs ...func
 			fmt.Fprintln(w)
 		}
 		fmt.Fprintln(w, text)
+	}
+}
+
+// isZero is a backport of reflect.Value.IsZero()
+func isZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return math.Float64bits(v.Float()) == 0
+	case reflect.Complex64, reflect.Complex128:
+		c := v.Complex()
+		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
+	case reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			if !v.Index(i).IsZero() {
+				return false
+			}
+		}
+		return true
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
+		return v.IsNil()
+	case reflect.String:
+		return v.Len() == 0
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			if !v.Field(i).IsZero() {
+				return false
+			}
+		}
+		return true
+	default:
+		// This should never happens, but will act as a safeguard for
+		// later, as a default value doesn't makes sense here.
+		panic(fmt.Sprintf("Value.IsZero: %v", v.Kind()))
 	}
 }
